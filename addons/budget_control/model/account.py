c6862dc354682e460a4c1e8b58894c9173a05d06
Cubic ERP <info@cubicerp.com>
2017-01-29 12:05:32 -0500

    [IMP] assets with out auto closing

---
+++ addons/budget_control/model/account.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from datetime import date, datetime
+
+# from openerp.osv import fields, osv, expression
+from openerp import models, fields, api, _
+from openerp.tools import ustr, DEFAULT_SERVER_DATE_FORMAT
+from openerp.tools.translate import _
+
+import openerp.addons.decimal_precision as dp
+
+
+class AccountAccount(models.Model):
+    _inherit = "account.account"
+
+    budget_post_ids = fields.Many2many('budget.position', 'account_budget_rel', 'account_id', 'budget_id',
+                                       string='Budget Positions')
+
+
+class AccountAnalyticAaccount(models.Model):
+    _inherit = "account.analytic.account"
+
+    budget_budget_line_ids = fields.One2many('budget.budget.lines', 'analytic_account_id', string='Budget Lines')
+
+
+class AccountPeriod(models.Model):
+    _inherit = 'account.period'
+
+    budget_period_id = fields.Many2one('budget.period', string='Budget Period')
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    budget_struct_id = fields.Many2one('budget.struct', 'Budget Struct', domain=[('type', '=', 'normal')])
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.invoice.line"
+
+    budget_struct_id = fields.Many2one('budget.struct', 'Budget Struct', domain=[('type', '=', 'normal')])

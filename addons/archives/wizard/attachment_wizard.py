a30d9ce850ad8e8957a070ff46a6589f90b81c28
peterpaul <ppit890819@gmail.com>
2017-02-16 14:57:10 -0500

    [update] archives: wizard attachment refactoring

--- addons/archives/wizard/attachment_wizard.py
+++ addons/archives/wizard/attachment_wizard.py
@@ -26,28 +26,18 @@ class AttachmentWizard(models.TransientModel):
     _name = "attachment.wizard"
 
     @api.model
-    def _compute_version_id_domain(self):
+    def _compute_domain_version_id(self):
         return [('document_id', '=', self._context.get('arch_document_id'))]
 
-    document_id = fields.Many2one('archives.document','Document',default=lambda self: self._context.get('active_id'))
+    def _compute_domain_attachment_id(self):
+        return [('id', '=', 0)]
 
-    user_id = fields.Many2one('res.users','Owner', default=lambda self: self._uid)
 
-    name = fields.Char('Attachment Name', required=True)
-
-    type_data = fields.Selection([('url', 'URL'), ('binary', 'Binary'), ],'Type', help="Binary File or URL", required=True, default='binary')
-
-    url = fields.Char('Url', size=1024)
-
-    data = fields.Binary(string='File Content')
-
-    version_id = fields.Many2one('archives.document.version','Document Version', domain=_compute_version_id_domain, default=lambda self: self.env['archives.document.version'].search([('document_id','=',self._context.get('arch_document_id'))], limit=1, order='date desc'))
-
-    description = fields.Text('Description')
+    attachment_id = fields.Many2one('ir.attachment', 'Attachment', domain=_compute_domain_attachment_id)
 
     version_new = fields.Boolean('New version ?', default=False)
 
-    version_number = fields.Integer('Version Number')
+    version_id = fields.Many2one('archives.document.version','Document Version', domain=_compute_domain_version_id, default=lambda self: self.env['archives.document.version'].search([('document_id','=',self._context.get('arch_document_id'))], limit=1, order='date desc'))
 
     @api.model
     def _default_version_last(self):
@@ -57,18 +47,8 @@ class AttachmentWizard(models.TransientModel):
     @api.multi
     def save_version(self):
 
-        attch_vals = {
-            'name': self.name,
-            'type': self.type_data,
-            'datas': self.data,
-            'url': self.url,
-            'user_id': self.user_id.id,
-            'description': self.description,
-        }
-        attch = self.env['ir.attachment'].create(attch_vals)
-
         if not self.version_new and self.version_id.id:
-            self.version_id.attachment_ids += attch
+            self.version_id.attachment_ids += self.attachment_id
         else:
             number = 1
             if self.version_id.id:
@@ -79,13 +59,13 @@ class AttachmentWizard(models.TransientModel):
                 'version_number': number,
                 'name': 'Version_'+str(number),
                 'date': datetime.datetime.now(),
-                'document_id': self.document_id.id,
+                'document_id': self._context.get('arch_document_id')
             }
 
             vers= self.env['archives.document.version'].create(version_vals)
-            vers.attachment_ids += attch
-            self.document_id.version_ids += vers
-
+            vers.attachment_ids += self.attachment_id
+            doc = self.env['archives.document'].browse(self._context.get('arch_document_id'))
+            doc.version_ids += vers
 
 
 

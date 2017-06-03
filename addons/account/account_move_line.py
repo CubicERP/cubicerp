9b522521302636bee4d301f3025d55eebdeb692e
Cubic ERP <info@cubicerp.com>
2017-06-03 13:20:27 -0500

    [IMP] accoount_move_line add force_move_line_quantity

--- addons/account/account_move_line.py
+++ addons/account/account_move_line.py
@@ -1333,7 +1333,7 @@ class account_move_line(osv.osv):
         vals['journal_id'] = vals.get('journal_id') or context.get('journal_id')
         vals['period_id'] = vals.get('period_id') or context.get('period_id')
         vals['date'] = vals.get('date') or context.get('date')
-        if 'quantity' in vals and vals['quantity'] and (vals.get('credit',0.0) or vals.get('debit',0.0)):
+        if not context.get('force_move_line_quantity', False) and 'quantity' in vals and vals['quantity'] and (vals.get('credit',0.0) or vals.get('debit',0.0)):
             vals['quantity'] = (-1.0 if vals.get('credit',False) else 1.0) * vals['quantity']
         if not move_id:
             if journal.centralisation:
